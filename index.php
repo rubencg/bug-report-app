7a0a4a6284bf84db2f923104dc906ebf0f909eef
Ruben Cardenas <rcardenas@tacitknowledge.com>
2020-07-26 14:04:52 -0500

    add app logs. Closes #6

--- index.php
+++ index.php
@@ -4,3 +4,7 @@ declare(strict_types = 1);
 
 require_once __DIR__.'/vendor/autoload.php';
 require_once __DIR__.'/Src/Exception/exception.php';
+
+$logger = new \App\Logger\Logger();
+$logger->log(\App\Logger\LogLevel::DEBUG, "THERE IS A DEBUG LOG", ['exception' => 'exception occurred']);
+$logger->info("User account created", ['id'=>'5']);
