47ef36de191064845dc5f8ac55607a75dc73ba52
Ruben Cardenas <rcardenas@tacitknowledge.com>
2020-07-25 20:18:44 -0500

    Create helper app class. Closes #2

---
+++ Src/Helpers/App.php
@@ -0,0 +1,64 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Helpers;
+
+use DateTimeInterface, Exception, DateTime, DateTimeZone;
+
+class App
+{
+    private $config = [];
+
+    public function __construct()
+    {
+        $this->config = Config::get('app');
+    }
+
+    /**
+     * Returns if debug mode is enabled
+     * @return bool
+     */
+    public function isDebugMode(): bool
+    {
+        $debug = $this->config['debug'];
+        return isset($debug) ? $debug : false;
+    }
+
+    /**
+     * Returns environment
+     * @return string
+     */
+    public function getEnvironment(): string
+    {
+        $env = $this->config['env'];
+        return isset($env) ? $env : 'production';
+    }
+
+    /**
+     * Returns Log path
+     * @return string
+     * @throws Exception if log path is not defined
+     */
+    public function getLogPath(): string
+    {
+        $log_path = $this->config['log_path'];
+        if(!isset($log_path)){
+            throw new Exception('Log path is not defined');
+        }
+        return $log_path;
+    }
+
+    /**
+     * Returns if php is running from console or browser
+     * @return bool
+     */
+    public function isRunningFromConsole(): bool
+    {
+        return php_sapi_name() == 'cli' || php_sapi_name() == 'phpbg';
+    }
+
+    public function getServerTime(): DateTimeInterface
+    {
+        return new DateTime('now', new DateTimeZone('America/Mexico_City'));
+    }
+}
