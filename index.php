9954b187f4b15e367a9078b6be69a32d78f5f23e
Ruben Cardenas <rcardenas@tacitknowledge.com>
2020-07-25 20:01:25 -0500

    Added config files. Closes #1

--- index.php
+++ index.php
@@ -2,4 +2,7 @@
 
 declare(strict_types = 1);
 
-require_once __DIR__.'vendor/autoload.php';
+require_once __DIR__.'/vendor/autoload.php';
+
+$config = \App\Helpers\Config::get('app');
+var_dump($config);
